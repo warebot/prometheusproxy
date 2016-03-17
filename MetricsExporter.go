971d64f58b0c6f27a70ff8b2287a491c205eb4d8
Mina Naguib <mnaguib@outbrain.com>
2016-03-17 10:13:47 -0400

    Adding TCP reconnect and some fault tolerance suuport

--- MetricsExporter.go
+++ MetricsExporter.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	dto "github.com/prometheus/client_model/go"
 	"net"
 	"time"
@@ -14,37 +15,78 @@ type MetricsExporter interface {
 type TCPMetricsExporter struct {
 	destAddr string
 	dataChan chan *dto.MetricFamily
+	conn     net.Conn
 }
 
-func (t TCPMetricsExporter) connect() net.Conn {
+func (t *TCPMetricsExporter) reconnect(established chan struct{}, quit chan struct{}) {
 	// Some type of backoff algorithm
 	seed := 5
 	for i := 1; i < 20000; i++ {
 		conn, err := net.Dial("tcp", t.destAddr)
 		if err == nil {
-			return conn
+			t.conn = conn
+			established <- struct{}{}
+		} else {
+			fmt.Println(err.Error())
 		}
 		time.Sleep(time.Second * time.Duration(i*seed))
 	}
-	return nil
+	quit <- struct{}{}
+
 }
 
-func (t TCPMetricsExporter) start() {
-	conn := t.connect()
-
-	if conn == nil {
-		Error.Println("failed to establish tcp connection: exhausted retries")
-		return
+func (t *TCPMetricsExporter) connect() net.Conn {
+	conn, err := net.Dial("tcp", t.destAddr)
+	if err != nil {
+		panic(err.Error())
 	}
+	return conn
+}
+
+func (t *TCPMetricsExporter) start() {
+	conn := t.connect()
+	t.conn = conn
+	ok := true
+
+	established := make(chan struct{})
+	quit := make(chan struct{})
+	exhausted := false
 
 	for {
 		m := <-t.dataChan
 		data, _ := json.Marshal(m)
-		_, err := conn.Write(data)
-		_, err = conn.Write([]byte("\n"))
-		if err != nil {
+		_, err := t.conn.Write(data)
+		_, err = t.conn.Write([]byte("\n"))
+
+		if err != nil && !exhausted {
 			Error.Println(err.Error())
-			conn = t.connect()
+			go t.reconnect(established, quit)
+			ok = false
+
+			for {
+				fmt.Println("Draining")
+				select {
+				case <-t.dataChan:
+				default:
+				}
+
+				select {
+				case <-established:
+					ok = true
+					break
+				case <-quit:
+					quit <- struct{}{}
+					exhausted = true
+					break
+				default:
+					continue
+				}
+				if ok {
+					break
+				}
+			}
+
 		}
+
 	}
 }
